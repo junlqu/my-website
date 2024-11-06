7b50a67035308ce043d78ff5ca65da92e444420d
Jun <17263194+junlqu@users.noreply.github.com>
2024-11-06 16:32:17 -0500

    feat: added header info

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,6 +1,35 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+
 import HeaderImage from "./HeaderImage";
 
 export default function MainHeader() {
+  const [scrollState, setScrollState] = useState(0);
+
+  const onScrollup = useCallback(() => {
+    const scrollY = window.scrollY || window.pageYOffset;
+    setScrollState(scrollY);
+    // If upscroll is detected, show the header
+    if (scrollY < scrollState) {
+      document.querySelector("#main-header").style = "top: 0;";
+    }
+    // If downscroll is detected, hide the header
+    else if (scrollY > scrollState) {
+      document.querySelector("#main-header").style = "top: -80px;";
+    }
+  });
+
+  useEffect(() => {
+    setScrollState(window.scrollY || window.pageYOffset);
+
+    window.addEventListener("scroll", onScrollup, {passive: true});
+
+    return () => {
+      window.removeEventListener("scroll", onScrollup, {passive: true});
+    };
+  }, [scrollState]);
+
   return (
     <header id="main-header" className="header scroll-fix wrapper">
       <HeaderImage />
