03729bb31c9d64ca278c985246266d95400429b1
Jun <17263194+junlqu@users.noreply.github.com>
2024-11-14 20:08:58 -0500

    feat: updated projects

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import LandingAboutMe from "cmp/landing/LandingAboutMe";
 import LandingIntro from "cmp/landing/LandingIntro";
 import LandingParallax from "cmp/landing/LandingParallax";
+import LandingProjects from "cmp/landing/LandingProjects";
 
 export default function Main() {
   useEffect(() => {
@@ -20,6 +21,7 @@ export default function Main() {
       <LandingIntro />
       <LandingParallax />
       <LandingAboutMe />
+      <LandingProjects />
       <div className="welcome-message-spacer" />
       <div className="welcome-message-spacer" />
       <div className="welcome-message-spacer" />
